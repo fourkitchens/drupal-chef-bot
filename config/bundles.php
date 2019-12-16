d1adda3c12ffefb5d4e1008129e6db4082b6f3eb
Michael Lutz <mlutz531@gmail.com>
2019-12-16 18:57:44 -0500

    Downgrade to SF4, install REST bundle and JMS serializer

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
