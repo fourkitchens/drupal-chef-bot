2e0cdd7eb92285a6f9adc595f878416d1abf84d5
Michael Lutz <mlutz531@gmail.com>
2019-12-28 13:02:28 -0500

    Initial Bot reponses

--- src/BotResponse.php
+++ src/BotResponse.php
@@ -2,6 +2,15 @@
 
 namespace App;
 
+/**
+ * Repostory of Bot response strings for use in SprintF
+ */
 class BotResponse {
+
+  // Basic user interaction strings.
+  public const OKAY = "Okay!";
+  public const OKAY_USER = "Okay, %s!";
+
+
 
 }
