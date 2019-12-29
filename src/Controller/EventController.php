e0ff127b54dfb207174f662a201ca9826f62c834
Michael Lutz <mlutz531@gmail.com>
2019-12-29 13:55:13 -0500

    Dispatch Symfony events for Slack events.

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,20 +3,27 @@
 namespace App\Controller;
 
 use App\Entity\EventLog;
+use App\Event\SlackEvent;
 use App\Utility\UrlVerification;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpKernel\Event\TerminateEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 /**
  * Class EventController
  *
  * @Route("/api", name="api_")
  */
-class EventController extends AbstractFOSRestController {
+class EventController extends AbstractFOSRestController implements LoggerAwareInterface {
 
   /**
    * The event log repository.
@@ -26,15 +33,44 @@ class EventController extends AbstractFOSRestController {
   protected $eventLogRepository;
 
   /**
+   * A Logger.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * The event dispatcher.
+   *
+   * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
+   */
+
+  protected $dispatcher;
+
+  /**
+   * The event to dispatch after the response has been sent.
+   *
+   * @var \App\Event\SlackEvent
+   */
+  protected $event;
+
+  /**
+   * The event name to dispatch after the response has been sent.
+   * @var string
+   */
+  protected $eventName;
+
+  /**
    * The entity manager.
    *
    * @var \Doctrine\ORM\EntityManagerInterface
    */
   protected $entityManager;
 
-  public function __construct(EntityManagerInterface $entity_manager) {
+  public function __construct(EntityManagerInterface $entity_manager, EventDispatcherInterface $dispatcher) {
     $this->eventLogRepository = $entity_manager->getRepository(EventLog::class);
     $this->entityManager = $entity_manager;
+    $this->dispatcher = $dispatcher;
   }
 
 
@@ -51,19 +87,48 @@ class EventController extends AbstractFOSRestController {
    * @throws \Exception
    */
   public function postEvent(Request $request) {
-    $params = json_decode($request->getContent());
+    $request_body_raw = $request->getContent();
+    $params = json_decode($request_body_raw);
     if ($params->type ?? '' === 'event_callback') {
       $event_type = $params->event->type ?? FALSE;
     }
     else {
       $event_type = $params->type ?? FALSE;
     }
-    if (!$type) {
+    if (!$event_type) {
+      $this->logger->error("Event Type not found in event callback. Request was {request}", ['request' => $request_body_raw]);
       return new Response('', Response::HTTP_BAD_REQUEST);
     }
-    if ($type === 'url_verification') {
+    if ($event_type === 'url_verification') {
+      $this->logger->debug("Handling URL verification for {request}", ['request' => $request_body_raw]);
       return UrlVerification::handle($request);
     }
+
+    $this->eventName = $event_type;
+    $this->event = new SlackEvent($params);
+    $this->dispatcher->addListener(KernelEvents::TERMINATE, [$this, 'onKernelTerminate']);
+
+    $this->logger->debug("Handling normal event - {request}", ['request' => $request_body_raw]);
     return new Response('');
+  }
+
+  /**
+   * Sets the logger.
+   *
+   * @param \Psr\Log\LoggerInterface $logger
+   */
+  public function setLogger(LoggerInterface $logger) {
+    $this->logger = $logger;
+  }
+
+  /**
+   * Process the event after sending the response.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\TerminateEvent $event
+   */
+  public function onKernelTerminate(TerminateEvent $event) {
+    $this->logger->debug("Sending slack event {event}", ['event' => $this->eventName]);
+    $this->dispatcher->dispatch($this->event);
+    $this->dispatcher->dispatch($this->event, $this->eventName);
   }
 }
--- src/Event/SlackEvent.php
+++ src/Event/SlackEvent.php
@@ -6,4 +6,136 @@ use Symfony\Contracts\EventDispatcher\Event;
 
 class SlackEvent extends Event {
 
+  /**
+   * The team id.
+   *
+   * @var string
+   */
+  protected $teamId;
+
+  /**
+   * The app id.
+   *
+   * @var string
+   */
+  protected $appId;
+
+  /**
+   * The event object.
+   *
+   * @var object
+   */
+  protected $event;
+
+  /**
+   * The event type.
+   *
+   * @var string
+   */
+  protected $type;
+
+  /**
+   * List of user ids inclded with the event.
+   *
+   * @var string[]
+   */
+  protected $authedUsers;
+
+  /**
+   * The unique slack event id.
+   *
+   * @var string
+   */
+  protected $eventId;
+
+  /**
+   * The event timestamp.
+   *
+   * @var string
+   */
+  protected $eventTime;
+
+  /**
+   * Constructs a SlackEvent object.
+   *
+   * @param object $event_wrapper
+   *   The event wrapper object.
+   */
+  public function __construct($event_wrapper) {
+    if (empty($event_wrapper->event)) {
+      throw new \InvalidArgumentException("Event object does not exist");
+    }
+    $this->teamId = $event_wrapper->team_id ?? '';
+    $this->appId = $event_wrapper->api_app_id ?? '';
+    $this->event = $event_wrapper->event;
+    $this->type = $event_wrapper->event->type ?? '';
+    $this->authedUsers = $event_wrapper->authed_users ?? [];
+    $this->eventId = $event_wrapper->event_id ?? '';
+    $this->eventTime = $event_wrapper->event_time ?? '';
+  }
+
+  /**
+   * Gets the value of $TeamId.
+   *
+   * @return string
+   */
+  public function getTeamId(): string {
+    return $this->teamId;
+  }
+
+  /**
+   * Gets the value of $AppId.
+   *
+   * @return string
+   */
+  public function getAppId(): string {
+    return $this->appId;
+  }
+
+  /**
+   * Gets the value of $Event.
+   *
+   * @return object
+   */
+  public function getEvent(): object {
+    return $this->event;
+  }
+
+  /**
+   * Gets the value of $Type.
+   *
+   * @return string
+   */
+  public function getType(): string {
+    return $this->type;
+  }
+
+  /**
+   * Gets the value of $AuthedUsers.
+   *
+   * @return string[]
+   */
+  public function getAuthedUsers(): array {
+    return $this->authedUsers;
+  }
+
+  /**
+   * Gets the value of $EventId.
+   *
+   * @return string
+   */
+  public function getEventId(): string {
+    return $this->eventId;
+  }
+
+  /**
+   * Gets the value of $EventTime.
+   *
+   * @return string
+   */
+  public function getEventTime(): string {
+    return $this->eventTime;
+  }
+
+
 }
